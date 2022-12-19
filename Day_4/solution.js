d4c4bf3b4fcc514a08a2ddafe7e8a3adc60f77f2
GeorgeTsif <george.tsiftsis@cwebd.co.uk>
2022-12-19 15:14:29 +0200

    2nd part

--- Day_4/solution.js
+++ Day_4/solution.js
@@ -1,5 +1,6 @@
 const input = require("fs").readFileSync("input.txt", "utf-8").split("\n");
-let fullOverlaps = 0
+let fullOverlaps = 0;
+let partOverlaps = 0;
 
 for (let i = 0; i < input.length; i++) {
 
@@ -10,11 +11,21 @@ for (let i = 0; i < input.length; i++) {
 
 	let pairs = input[i].match(/(\d+)/g);
 	pairs = pairs.map((n) => parseInt(n));
-	if ((pairs[0] <= pairs[2] && pairs[1] >= pairs[3]) ||
-		(pairs[0] >= pairs[2] && pairs[1] <= pairs[3])
-	)
-		fullOverlaps++;
 
+	let r1 = [pairs[0], pairs[1]];
+	let r2 = [pairs[2], pairs[3]];
+	let maxNumber = (a) => Math.max(a);
+	let minNumber = (a) => Math.min(a);
+
+	if ((pairs[0] <= pairs[2] && pairs[1] >= pairs[3]) || (pairs[0] >= pairs[2] && pairs[1] <= pairs[3]))
+		fullOverlaps++;
+	else if (r1.some((n) => r2.includes(n))) partOverlaps++;
+	else if (
+		(maxNumber(r1) >= minNumber(r2) && maxNumber(r1) <= maxNumber(r2)) ||
+		(maxNumber(r2) >= minNumber(r1) && maxNumber(r2) <= maxNumber(r1))
+	)
+		partOverlaps++;
 }
 
 console.log(`The first solution is ${fullOverlaps}`);
+console.log(`The second solution is ${partOverlaps + fullOverlaps}`);
